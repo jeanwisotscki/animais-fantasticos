cedf2190fdb4e0467d6cde9433140ef3c094c3ff
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-02-22 13:30:42 -0300

    função de fetch bitcoin refatorada

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,11 +1,11 @@
-export default function initFetchBitcoin() {}
-
-fetch("https://blockchain.info/ticker")
-  .then((res) => res.json())
-  .then((bitcoin) => {
-    const btcPreco = document.querySelector(".btc-preco");
-    btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
-  })
-  .catch((erro) => {
-    console.log("Erro. Tente novamente mais tarde. ", erro);
-  });
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
+    })
+    .catch((erro) => {
+      console.log("Erro. Tente novamente mais tarde. ", erro);
+    });
+}
